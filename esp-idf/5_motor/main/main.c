c1a211a22fe3fd6b92b986c7f3db753fbb4e5b27
ShotaAk <macakasit@gmail.com>
2020-02-08 15:03:55 +0900

    update motor main

--- esp-idf/5_motor/main/main.c
+++ esp-idf/5_motor/main/main.c
@@ -8,95 +8,103 @@
 #include "soc/gpio_struct.h"
 #include "driver/mcpwm.h"
 
+static const mcpwm_unit_t UNIT = MCPWM_UNIT_0;
+static const gpio_num_t GPIO_IN1 = GPIO_NUM_32;
+static const gpio_num_t GPIO_IN2 = GPIO_NUM_33;
 
-#define GPIO_PWM0A_OUT 32
-#define GPIO_PWM0B_OUT 33
+static const gpio_num_t SIGNAL_IN1 = MCPWM0A;
+static const gpio_num_t SIGNAL_IN2 = MCPWM0B;
+static const mcpwm_timer_t TIMER = MCPWM_TIMER_0;
 
-static mcpwm_timer_t MC_TIMER;
+static const mcpwm_operator_t OPR_IN1 = MCPWM_OPR_A;
+static const mcpwm_operator_t OPR_IN2 = MCPWM_OPR_B;
 
-static void mcpwm_example_gpio_initialize()
+static const mcpwm_duty_type_t DUTY_MODE = MCPWM_DUTY_MODE_0; // Active high duty
+
+
+static void drive_forward(mcpwm_timer_t timer_num , float duty)
 {
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
+    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN2);
+    mcpwm_set_duty(UNIT, timer_num, OPR_IN1, duty);
+    mcpwm_set_duty_type(UNIT, timer_num, OPR_IN1, DUTY_MODE);
 }
 
-/**
- * @brief motor moves in forward direction, with duty cycle = duty %
- */
-static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+static void drive_backward(mcpwm_timer_t timer_num , float duty)
 {
-    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
-    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
-    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0); //call this each time, if operator was previously in low/high state
+    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN1);
+    mcpwm_set_duty(UNIT, timer_num, OPR_IN2, duty);
+    mcpwm_set_duty_type(UNIT, timer_num, OPR_IN2, DUTY_MODE);
 }
 
-/**
- * @brief motor moves in backward direction, with duty cycle = duty %
- */
-static void brushed_motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+static void drive_brake(mcpwm_timer_t timer_num)
 {
-    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
-    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
-    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0); 
+    mcpwm_set_signal_high(UNIT, timer_num, OPR_IN1);
+    mcpwm_set_signal_high(UNIT, timer_num, OPR_IN2);
 }
 
-/**
- * @brief motor stop
- */
-static void brushed_motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
+static void drive_stop(mcpwm_timer_t timer_num)
 {
-    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
-    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN1);
+    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN2);
 }
 
-static void motorDrive(float duty){
-    // dutyを±100に加工する
-    if(duty < -100){
-        duty = -100;
-    }else if(duty > 100){
-        duty = 100;
+static void motor_drive(const unsigned int go_back, const float duty)
+{
+    const float DUTY_MAX = 100;
+    const float DUTY_MIN = 5;
+
+    float target_duty = duty;
+    if(target_duty > DUTY_MAX){
+        target_duty = DUTY_MAX;
     }
-    
-    if(duty < 0){
-        // dutyがマイナスのときは逆回転
-        duty *= -1.0; // dutyの符号を正にする
-        brushed_motor_backward(MCPWM_UNIT_0, MC_TIMER, duty);
+
+    if(target_duty < DUTY_MIN){
+        drive_brake(TIMER);
     }else{
-        brushed_motor_forward(MCPWM_UNIT_0, MC_TIMER, duty);
+        if(go_back){
+            drive_backward(TIMER, target_duty);
+        }else{
+            drive_forward(TIMER, target_duty);
+        }
     }
+}
+
+static void motor_off(void)
+{
+    drive_stop(TIMER);
 }
 
 void app_main()
 {
-    //1. mcpwm gpio initialization
-    mcpwm_example_gpio_initialize();
+    mcpwm_gpio_init(UNIT, SIGNAL_IN1, GPIO_IN1);
+    mcpwm_gpio_init(UNIT, SIGNAL_IN2, GPIO_IN2);
 
-    //2. initial mcpwm configuration
     mcpwm_config_t pwm_config;
-    pwm_config.frequency = 100*1000;    //frequency = 100kHz,
-    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.frequency = 100*1000; // frequency = 100kHz,
+    pwm_config.cmpr_a = 0; // duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0; // duty cycle of PWMxb = 0
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
-    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    pwm_config.duty_mode = DUTY_MODE;
 
-    MC_TIMER = MCPWM_TIMER_0;
+    mcpwm_init(UNIT, TIMER, &pwm_config);
 
-    mcpwm_init(MCPWM_UNIT_0, MC_TIMER, &pwm_config);    //Configure PWM0A & PWM0B with above settings
-
+    motor_off();
     while (1) {
-        const int WAIT_TIME = 3000;
+        const int WAIT_TIME = 200;
+        const float TARGET_DUTY_MAX = 100;
+        const float TARGET_DUTY_MIN = 50;
 
-        brushed_motor_stop(MCPWM_UNIT_0, MC_TIMER);
-        vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
+        for(int go_back=0; go_back<=1; go_back++){
+            for(int duty=TARGET_DUTY_MIN; duty<TARGET_DUTY_MAX; duty++){
+                motor_drive(go_back, duty);
+                vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
+            }
 
-        motorDrive(80);
-        vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
-
-        motorDrive(0);
-        vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
-
-        motorDrive(-80);
-        vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
+            for(int duty=TARGET_DUTY_MAX; duty>TARGET_DUTY_MIN; duty--){
+                motor_drive(go_back, duty);
+                vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
+            }
+        }
     }
 }
 
