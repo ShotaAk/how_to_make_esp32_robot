8d99482d348241c9f39752cc3060e2c952337291
ShotaAk <macakasit@gmail.com>
2020-02-09 17:55:54 +0900

    Add encoder example

---
+++ esp-idf/6_encoder/main/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/pcnt.h"
+
+void app_main()
+{
+    static const pcnt_unit_t UNIT = PCNT_UNIT_0;
+
+    pcnt_config_t pcnt_config = {
+        .pulse_gpio_num = GPIO_NUM_32,
+        .ctrl_gpio_num = GPIO_NUM_33,
+        .channel = PCNT_CHANNEL_0,
+        .unit = UNIT,
+        .pos_mode = PCNT_COUNT_INC,
+        .neg_mode = PCNT_COUNT_DIS,
+        .lctrl_mode = PCNT_MODE_REVERSE,
+        .hctrl_mode = PCNT_MODE_KEEP,
+        .counter_h_lim = 20000,
+        .counter_l_lim = -20000,
+    };
+    pcnt_unit_config(&pcnt_config);
+
+    pcnt_filter_enable(UNIT);
+
+    // Pulse Per Moter Revolution
+    static const int PPMR = 3;
+    // Pulse Per Shaft Revolution
+    static const int PPSR = 1300;
+    static const int WAIT_TIME_MS = 100;
+    static const float WAIT_TIME_SEC = WAIT_TIME_MS * 0.001;
+
+    while (1) {
+        int16_t count = 0;
+        pcnt_get_counter_value(UNIT, &count);
+
+        float motor_speed = ((float)count / PPMR) / (WAIT_TIME_SEC);
+        float shaft_speed = ((float)count / PPSR) / (WAIT_TIME_SEC);
+
+        printf("Count:%d\tMotorSpped:%fRPS\tShaftSpeed:%fRPS\n", 
+                count, motor_speed, shaft_speed);
+        pcnt_counter_clear(UNIT);
+        vTaskDelay(pdMS_TO_TICKS(WAIT_TIME_MS));
+    }
+}
