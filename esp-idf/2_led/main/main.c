d3c8f5daafec0c536b787767eb667bb7ede84b9e
ShotaAk <macakasit@gmail.com>
2020-02-23 16:11:39 +0900

    Update led main.c

--- esp-idf/2_led/main/main.c
+++ esp-idf/2_led/main/main.c
@@ -1,34 +1,41 @@
-/* GPIO Example
+// Ref: https://docs.espressif.com/projects/esp-idf/en/v3.3.1/api-reference/peripherals/gpio.html
+//      https://github.com/espressif/esp-idf/blob/release/v3.3/components/driver/include/driver/gpio.h
+//      https://github.com/espressif/esp-idf/blob/release/v3.3/components/driver/gpio.c
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "freertos/FreeRTOS.h"
 #include "driver/gpio.h"
-
-#define GPIO_INPUT_IO 13
-#define GPIO_OUTPUT_IO 23
 
 void app_main()
 {
+    static const gpio_num_t GPIO_OUTPUT_IO = GPIO_NUM_23;
+    static const gpio_num_t GPIO_INPUT_IO = GPIO_NUM_13;
+
     gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    // 割り込みをしない
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    // 出力モード
     io_conf.mode = GPIO_MODE_OUTPUT;
+    // 設定したいピンのビットマスク
+    // 1ULLはunsigned long long(64bit)で1を表現する
+    // 23ピンを設定するので、ビットマスクは下記のようになる
+    // 上位32ビット：MSB|0000 0000|0000 0000|0000 0000|0000 0000|
+    // 下位32ビット：    0000 0000|1000 0000|0000 0000|0000 0000|LSB
     io_conf.pin_bit_mask = (1ULL<<GPIO_OUTPUT_IO);
+    // 内部プルダウンしない
     io_conf.pull_down_en = 0;
+    // 内部プルダウンしない
     io_conf.pull_up_en = 0;
+    // 設定をセットする
     gpio_config(&io_conf);
 
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    // 入力ピンの設定
     io_conf.mode = GPIO_MODE_INPUT;
+    // 設定したいピンのビットマスク
     io_conf.pin_bit_mask = (1ULL<<GPIO_INPUT_IO);
+    // 内部プルダウンする
+    // ESP32外部にプルダウン抵抗を接続しているので、
+    // 内部プルダウンはしなくてもOK
     io_conf.pull_down_en = 1;
+    // 設定をセットする
     gpio_config(&io_conf);
 
     while(1) {
