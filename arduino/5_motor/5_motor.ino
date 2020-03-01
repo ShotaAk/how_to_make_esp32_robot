29ae9bc07c1ed89926cda6334bf406ee9257b131
ShotaAk <macakasit@gmail.com>
2020-03-01 19:46:16 +0900

    update motor arduino

--- arduino/5_motor/5_motor.ino
+++ arduino/5_motor/5_motor.ino
@@ -1,102 +1,82 @@
 // Ref: https://github.com/espressif/arduino-esp32/blob/1.0.4/tools/sdk/include/driver/driver/mcpwm.h
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <driver/mcpwm.h>
 
 static const mcpwm_unit_t UNIT = MCPWM_UNIT_0;
-static const gpio_num_t GPIO_IN1 = GPIO_NUM_32;
-static const gpio_num_t GPIO_IN2 = GPIO_NUM_33;
-
-static const mcpwm_io_signals_t SIGNAL_IN1 = MCPWM0A;
-static const mcpwm_io_signals_t SIGNAL_IN2 = MCPWM0B;
 static const mcpwm_timer_t TIMER = MCPWM_TIMER_0;
-
 static const mcpwm_operator_t OPR_IN1 = MCPWM_OPR_A;
 static const mcpwm_operator_t OPR_IN2 = MCPWM_OPR_B;
+static const mcpwm_duty_type_t DUTY_MODE = MCPWM_DUTY_MODE_0; // アクティブハイ
 
-static const mcpwm_duty_type_t DUTY_MODE = MCPWM_DUTY_MODE_0; // Active high duty
-
-
-static void drive_forward(mcpwm_timer_t timer_num , float duty)
-{
-    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN2);
-    mcpwm_set_duty(UNIT, timer_num, OPR_IN1, duty);
-    mcpwm_set_duty_type(UNIT, timer_num, OPR_IN1, DUTY_MODE);
+static void drive_forward(float duty){
+    // モータ正転
+    mcpwm_set_signal_low(UNIT, TIMER, OPR_IN2);
+    mcpwm_set_duty(UNIT, TIMER, OPR_IN1, duty);
+    // set_signal_low/highを実行した後は、毎回set_duty_typeを実行すること
+    mcpwm_set_duty_type(UNIT, TIMER, OPR_IN1, DUTY_MODE);
 }
 
-static void drive_backward(mcpwm_timer_t timer_num , float duty)
-{
-    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN1);
-    mcpwm_set_duty(UNIT, timer_num, OPR_IN2, duty);
-    mcpwm_set_duty_type(UNIT, timer_num, OPR_IN2, DUTY_MODE);
+static void drive_backward(float duty){
+    mcpwm_set_signal_low(UNIT, TIMER, OPR_IN1);
+    mcpwm_set_duty(UNIT, TIMER, OPR_IN2, duty);
+    // set_signal_low/highを実行した後は、毎回set_duty_typeを実行すること
+    mcpwm_set_duty_type(UNIT, TIMER, OPR_IN2, DUTY_MODE);
 }
 
-static void drive_brake(mcpwm_timer_t timer_num)
-{
-    mcpwm_set_signal_high(UNIT, timer_num, OPR_IN1);
-    mcpwm_set_signal_high(UNIT, timer_num, OPR_IN2);
+static void drive_brake(void){
+    // モータブレーキ
+    mcpwm_set_signal_high(UNIT, TIMER, OPR_IN1);
+    mcpwm_set_signal_high(UNIT, TIMER, OPR_IN2);
 }
 
-static void drive_stop(mcpwm_timer_t timer_num)
-{
-    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN1);
-    mcpwm_set_signal_low(UNIT, timer_num, OPR_IN2);
-}
-
-static void motor_drive(const unsigned int go_back, const float duty)
-{
-    const float DUTY_MAX = 100;
-    const float DUTY_MIN = 5;
-
-    float target_duty = duty;
-    if(target_duty > DUTY_MAX){
-        target_duty = DUTY_MAX;
-    }
-
-    if(target_duty < DUTY_MIN){
-        drive_brake(TIMER);
+static void motor_drive(const float duty, const int go_back){
+    if(go_back){
+        drive_backward(duty);
     }else{
-        if(go_back){
-            drive_backward(TIMER, target_duty);
-        }else{
-            drive_forward(TIMER, target_duty);
-        }
+        drive_forward(duty);
     }
-}
-
-static void motor_off(void)
-{
-    drive_stop(TIMER);
 }
 
 
 void setup() {
+    // GPIOの割り当て
+    mcpwm_gpio_init(UNIT, MCPWM0A, GPIO_NUM_33);
+    mcpwm_gpio_init(UNIT, MCPWM0B, GPIO_NUM_25);
 
-    mcpwm_gpio_init(UNIT, SIGNAL_IN1, GPIO_IN1);
-    mcpwm_gpio_init(UNIT, SIGNAL_IN2, GPIO_IN2);
-
+    // MCPWMの設定
     mcpwm_config_t pwm_config;
-    pwm_config.frequency = 100*1000; // frequency = 100kHz,
+    // PWM周波数は10*1000 Hz = 10 kHz
+    pwm_config.frequency = 10*1000;
+    // アップカウンタ
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    // デューティモードはアクティブハイ
     pwm_config.duty_mode = DUTY_MODE;
-
     mcpwm_init(UNIT, TIMER, &pwm_config);
-
-    motor_off();
+    // ブレーキをかけるが、この後のwhileでモータを回すので、特に意味はない
+    drive_brake();
 }
 
 void loop() {
-    const int WAIT_TIME = 200;
-    const float TARGET_DUTY_MAX = 100;
-    const float TARGET_DUTY_MIN = 50;
-
-    for(int go_back=0; go_back<=1; go_back++){
-        for(int duty=TARGET_DUTY_MIN; duty<TARGET_DUTY_MAX; duty++){
-            motor_drive(go_back, duty);
-            vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
-        }
-
-        for(int duty=TARGET_DUTY_MAX; duty>TARGET_DUTY_MIN; duty--){
-            motor_drive(go_back, duty);
-            vTaskDelay(WAIT_TIME / portTICK_RATE_MS);
+    const int WAIT_TIME_MS = 100;
+    const int DUTY_STEP = 1;
+    const int DUTY_MAX = 100;
+    // 使用するモータはデューティ比50%以下で動かなかった
+    const int DUTY_MIN = 50;
+    while (1) {
+        // 正転・逆転を切り替える
+        for(int go_back=0; go_back<=1; go_back++){
+            // デューティ比を増加させる（加速）
+            for(int duty=DUTY_MIN; duty<DUTY_MAX; duty+=DUTY_STEP){
+                motor_drive(duty, go_back);
+                vTaskDelay(WAIT_TIME_MS / portTICK_RATE_MS);
+            }
+        
+            // デューティ比を減少させる（減速）
+            for(int duty=DUTY_MAX; duty>=DUTY_MIN; duty-=DUTY_STEP){
+                motor_drive(duty, go_back);
+                vTaskDelay(WAIT_TIME_MS / portTICK_RATE_MS);
+            }
         }
     }
 }
