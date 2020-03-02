b3ab9e4bb05217833a392434c80160b3a4b77a18
ShotaAk <macakasit@gmail.com>
2020-03-03 01:21:52 +0900

    update bluetooth main.c

--- esp-idf/7_bluetooth/main/main.c
+++ esp-idf/7_bluetooth/main/main.c
@@ -648,17 +648,13 @@ void app_main()
     }
 
     // LチカのためのGPIO設定
-
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = (1ULL<<GPIO_OUTPUT_IO);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
     while(1) {
         gpio_set_level(GPIO_OUTPUT_IO, flag_led_on);
         vTaskDelay(100 / portTICK_RATE_MS);
     }
-    // return;
 }
